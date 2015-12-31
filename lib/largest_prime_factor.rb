f3c002a1b1d6013a751b12bbb1222e3b5daa7f8a
David Westberry <n30r3b3l@gmail.com>
2015-12-31 04:51:22 +0000

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,24 @@
 # Enter your procedural solution here!
+def largest_prime_factor(n)
+  factors = get_small_factors(n)
+  factors.each do |factor|
+    if get_small_factors(factor).empty?
+      return factor
+    end
+  end
+end
+
+def get_small_factors(n)
+  limit = Math.sqrt(n).floor
+  i = limit
+  lpf = 0
+  factors = Array.new
+  while i > 1
+    if n % i == 0
+      factors << i
+    end
+    i -= 1
+  end
+  return factors
+end
+puts "#{largest_prime_factor(26)}"
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,29 @@
 # Enter your object-oriented solution here!
+class LargestPrimeFactor
+  def initialize(n)
+    @number = n
+  end
+
+  def number
+    factors = get_small_factors(@number)
+    factors.each do |factor|
+      if get_small_factors(factor).empty?
+        return factor
+      end
+    end
+  end
+
+  def get_small_factors(n)
+    limit = Math.sqrt(n).floor
+    i = limit
+    lpf = 0
+    factors = Array.new
+    while i > 1
+      if n % i == 0
+        factors << i
+      end
+      i -= 1
+    end
+    return factors
+  end
+end
